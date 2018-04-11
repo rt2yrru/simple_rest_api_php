92cc17b6d13cf48e1c9ce0509c6b65d3cd1d3d01
siddht4 <dhar.75.ap@gmail.com>
2018-04-11 13:46:50 +0530

    Create headers.php

---
+++ headers.php
@@ -0,0 +1,10 @@
+<?php
+ header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+
+header("Content-Type:application/jsonp");
+header("HTTP/1.1 ") ;
+
+?>
